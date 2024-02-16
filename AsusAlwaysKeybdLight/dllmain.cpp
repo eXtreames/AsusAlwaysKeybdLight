56b14770f1ba93da6133dd28e8a82de93555669c
eXtreames <hellextreames@gmail.com>
2024-02-16 14:44:09 +0300

    Добавьте файлы проекта.

---
+++ AsusAlwaysKeybdLight/dllmain.cpp
@@ -0,0 +1,130 @@
+﻿/*------------------------------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <Windows.h>
+
+#define MIN_HOOK_IMPLEMENTATION
+#include "MinHook.h"
+
+/*------------------------------------------------------------------------------------------------*/
+
+PBYTE scan_memory(PBYTE base_address, PCHAR signature, PCHAR mask);
+void print_debug(const char* fmt, ...);
+
+/*------------------------------------------------------------------------------------------------*/
+
+UINT64(*original_send_lightning)(UINT64, UINT64, UINT64, UINT64) = { };
+static UINT64 naked_send_lightning(UINT64 a1, UINT64 a2, UINT64 a3, UINT64 a4)
+{
+	print_debug("[ info ] intercepted %llX %llX %llX %llX.\n", a1, a2, a3, a4);
+	if (a2 == 1 && a3 == 1 && a4 == 1) // enable lightning after windows waked up
+	{
+		return original_send_lightning(a1, a2, a3, a4);
+	}
+	if (a3 == 1 && a4 > 0x1000) // manual change lightning
+	{
+		return original_send_lightning(a1, a2, a3, a4);
+	}
+
+	print_debug("[ info ] fxcked %llX %llX %llX %llX.\n", a1, a2, a3, a4);
+	return NULL;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+static void main_thread()
+{
+    auto main_module = reinterpret_cast<PBYTE>(GetModuleHandle(L"AsusOptimization.exe"));
+    if (!main_module)
+    {
+		print_debug("[ error ] no main_module.\n");
+        return;
+    }
+
+	auto send_lightning = scan_memory(main_module, const_cast<PCHAR>("\x33\xFF\x45\x8B\xF0"), const_cast<PCHAR>("xxxxx"));
+	if (!send_lightning)
+	{
+		print_debug("[ error ] no send_lightning.\n");
+		return;
+	}
+	send_lightning -= 0x21;
+
+	MH_Initialize();
+	MH_CreateHook(send_lightning, &naked_send_lightning, reinterpret_cast<void**>(&original_send_lightning));
+	MH_EnableHook(send_lightning);
+
+	print_debug("[ info ] hooked.\n");
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+int __stdcall DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+        auto thread = CreateThread(NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(main_thread), nullptr, NULL, NULL);
+        if (thread)
+        {
+            CloseHandle(thread);
+        }
+        else
+        {
+			print_debug("[ error ] thread creation failed.");
+        }
+    }
+    return TRUE;
+}
+
+/*-----------------------------------------------------------------------------------------------*/
+
+PBYTE scan_memory(PBYTE base_address, PCHAR signature, PCHAR mask)
+{
+	auto _compare = [](PCHAR data, PCHAR sig, PCHAR mask) -> bool
+		{
+			for (; *mask; mask += 1, data += 1, sig += 1)
+			{
+				if (*mask == 'x' && *data != *sig)
+				{
+					return false;
+				}
+			}
+			return true;
+		};
+
+	SIZE_T mask_size = strlen(mask);
+	PBYTE result = { };
+
+	PIMAGE_NT_HEADERS headers = reinterpret_cast<PIMAGE_NT_HEADERS>(base_address + reinterpret_cast<PIMAGE_DOS_HEADER>(base_address)->e_lfanew);
+	PIMAGE_SECTION_HEADER sections = IMAGE_FIRST_SECTION(headers);
+
+	for (DWORD section_it = NULL; section_it < headers->FileHeader.NumberOfSections && !result; ++section_it)
+	{
+		PIMAGE_SECTION_HEADER section = &sections[section_it];
+
+		PCHAR virtualaddress = reinterpret_cast<PCHAR>(base_address + section->VirtualAddress);
+		DWORD virtualsize = static_cast<DWORD>(section->Misc.VirtualSize - mask_size);
+
+		for (ULONG i = NULL; i <= virtualsize; ++i)
+		{
+			if (_compare(&virtualaddress[i], signature, mask))
+			{
+				result = reinterpret_cast<PBYTE>(&virtualaddress[i]);
+				break;
+			}
+		}
+	}
+	return result;
+}
+
+void print_debug(const char* fmt, ...)
+{
+	char buffer[0x256] = { };
+
+	va_list ap = { }; va_start(ap, fmt);
+	vsprintf_s(buffer, fmt, ap);
+	va_end(ap);
+
+	return OutputDebugStringA(buffer);
+}
+
+/*------------------------------------------------------------------------------------------------*/
---
+++ Injector/Injector.cpp
@@ -0,0 +1,133 @@
+﻿/*------------------------------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+/*------------------------------------------------------------------------------------------------*/
+
+const char* dll_path = "C:\\Windows\\System32\\asus_always_keybd_light.dll";
+int wait_asus_process();
+bool enable_debug_priveleges();
+
+/*------------------------------------------------------------------------------------------------*/
+
+int main()
+{
+    int asus_process_id = wait_asus_process();
+    if (!asus_process_id)
+    {
+        MessageBox(NULL, L"keybd_light", L"find process failed.", NULL);
+        return NULL;
+    }
+    enable_debug_priveleges();
+
+    auto handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, static_cast<DWORD>(asus_process_id));
+    if (!handle)
+    {
+        MessageBox(NULL, L"keybd_light", L"open process failed.", NULL);
+        return NULL;
+    }
+
+    void* path_buffer = VirtualAllocEx(handle, nullptr, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (!path_buffer)
+    {
+        MessageBox(NULL, L"keybd_light", L"allocation failed.", NULL);
+        return NULL;
+    }
+
+    if (!WriteProcessMemory(handle, path_buffer, dll_path, strlen(dll_path) + 1, nullptr))
+    {
+        MessageBox(NULL, L"keybd_light", L"write failed.", NULL);
+        return NULL;
+    }
+
+    HANDLE thread = CreateRemoteThread(handle, NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(&LoadLibraryA), path_buffer, NULL, NULL);
+    if (!thread)
+    {
+        MessageBox(NULL, L"keybd_light", L"execute thread failed.", NULL);
+        return NULL;
+    }
+
+    CloseHandle(thread);
+    CloseHandle(handle);
+
+    return 0;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+int wait_asus_process()
+{
+    auto get_process = [](const wchar_t* process_name) -> int
+        {
+            HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+            if (snapshot == INVALID_HANDLE_VALUE) 
+            {
+                return NULL;
+            }
+
+            PROCESSENTRY32 entry = { };
+            entry.dwSize = sizeof(entry);
+
+            if (!Process32First(snapshot, &entry))
+            {
+                CloseHandle(snapshot);
+                return NULL;
+            }
+
+            do
+            {
+                if (wcscmp(entry.szExeFile, process_name) == 0) 
+                {
+                    CloseHandle(snapshot);
+                    return entry.th32ProcessID;
+                }
+            } while (Process32Next(snapshot, &entry));
+
+            CloseHandle(snapshot);
+            return NULL;
+        };
+
+    while (true)
+    {
+        auto result = get_process(L"AsusOptimization.exe");
+        if (result)
+        {
+            return result;
+        }
+        Sleep(1000);
+    }
+    return NULL;
+}
+
+bool enable_debug_priveleges()
+{
+    HANDLE token = 0;
+    TOKEN_PRIVILEGES new_priveleges = { };
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &token))
+    {
+        return false;
+    }
+    if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &new_priveleges.Privileges[0].Luid))
+    {
+        CloseHandle(token);
+        return false;
+    }
+
+    new_priveleges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    new_priveleges.PrivilegeCount = 1;
+
+    if (!AdjustTokenPrivileges(token, FALSE, &new_priveleges, 0, NULL, NULL))
+    {
+        CloseHandle(token);
+        return false;
+    }
+
+    CloseHandle(token);
+    return true;
+}
+
+/*------------------------------------------------------------------------------------------------*/
